8ff60eeb95610cadbde1ebaebd224a6cb2848d64
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 <behani.julien@gmxfr>
2026-02-09 21:15:51 +0100

    fixed compilation and start of borne

--- borne_arcade/compilation.sh
+++ borne_arcade/compilation.sh
@@ -1,20 +1,35 @@
-#!/bin/bash
+﻿#!/bin/bash
 
-echo "Compilation du menu de la borne d'arcade"
-echo "Veuillez patienter"
-javac -cp .:/home/pi/git/MG2D *.java
+if [ -f "../MG2D.jar" ]; then
+   echo "MG2D.jar exists."
+   DEPENDENCIES="../"
+else
+   if [ -d "~/git/MG2D" ]; then
+      echo "MG2D folder exists in user’s home."
+      DEPENDENCIES="~/git/MG2D/"
+   else
+      echo "No MG2D dependency found"
+	DEPENDENCIES=null
+   fi
+fi
 
-cd projet
 
+if [ -n "$DEPENDENCIES" ]; then
+   echo "Compilation du menu de la borne d'arcade"
+   echo "Veuillez patienter"
+   javac -cp .:$FOLDER *.java
 
-#PENSER A REMETTRE COMPILATION JEUX!!!
-for i in *
-do
-    cd $i
-    echo "Compilation du jeu "$i
-    echo "Veuillez patienter"
-    javac -cp .:../..:/home/pi/git/MG2D *.java
-    cd ..
-done
+   cd projet
+
+   #PENSER A REMETTRE COMPILATION JEUX!!!
+   for i in *
+   do
+       cd $i
+       echo "Compilation du jeu "$i
+       echo "Veuillez patienter"
+       javac -cp .:../..:$FOLDER *.java
+       cd ..
+   done
+fi
 
 cd ..
--- borne_arcade/lancerBorne.sh
+++ borne_arcade/lancerBorne.sh
@@ -2,22 +2,47 @@
 
 setxkbmap borne
 
-cd /home/pi/git/borne_arcade
-echo "nettoyage des répertoires"
-echo "Veuillez patienter"
-./clean.sh
-./compilation.sh
+
+if [ -f "../MG2D.jar" ]; then
+   echo "MG2D.jar exists."
+   DEPENDENCIES="../"
+else
+   if [ -d "~/git/MG2D" ]; then
+      echo "MG2D folder exists in user’s home."
+      DEPENDENCIES="~/git/MG2D/"
+   else
+      echo "No MG2D dependency found"
+	DEPENDENCIES=null
+   fi
+fi
+
+FOLDER="~/git/borne_arcade"
+
+if [-d "$FOLDER"]; then
+   cd $FOLDER
+   echo "nettoyage des répertoires"
+   echo "Veuillez patienter"
+   ./clean.sh
+   ./compilation.sh
+else
+   echo "Aucun dossier $FOLDER trouvé, création du dossier"
+   mkdir -p $FOLDER
+   cd $FOLDER
+   ./compilation.sh
+fi
 
 echo "Lancement du  Menu"
 echo "Veuillez patienter"
 
-java -cp .:/home/pi/git/MG2D Main
+if [ -n "$DEPENDENCIES" ]; then
+   java -cp .:$DEPENDENCIES Main
+fi
 
 ./clean.sh
 
 for i in {30..1}
 do
-    echo Extinction de la borne dans $i secondes
+    echo "Extinction de la borne dans $i secondes"
     sleep 1
 done
 
