3ff9e24e1d5cf44b51814b6e7103d47343fccd73
kinator <behani.julien@gmxfr>
2026-02-10 20:17:55 +0100

    fixed missing image on menu crash

--- borne_arcade/BoiteImage.java
+++ borne_arcade/BoiteImage.java
@@ -1,7 +1,9 @@
 import MG2D.geometrie.Point;
 import MG2D.geometrie.Rectangle;
 import MG2D.geometrie.Texture;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class BoiteImage extends Boite{
 
@@ -9,6 +11,13 @@ public class BoiteImage extends Boite{
 
     BoiteImage(Rectangle rectangle, String image) {
 	super(rectangle);
+        Path filePath = Paths.get(image+"/photo_small.png");
+	if (Files.exists(filePath)) {
+	    this.image = new Texture(image+"/photo_small.png", new Point(760, 648));
+	    //this.image.setTaille(400, 320);
+	} else {
+	    this.image.setImg("./img/synave.png");
+	}
 	this.image = new Texture(image+"/photo_small.png", new Point(760, 648));
     }
 
@@ -17,8 +26,12 @@ public class BoiteImage extends Boite{
     }
 
     public void setImage(String chemin) {
-	this.image.setImg(chemin+"/photo_small.png");
-	//this.image.setTaille(400, 320);
+        Path filePath = Paths.get(chemin+"/photo_small.png");
+	if (Files.exists(filePath)) {
+	    this.image.setImg(chemin+"/photo_small.png");
+	    //this.image.setTaille(400, 320);
+	} else {
+	    this.image.setImg("./img/synave.png");
+	}
     }
-
 }
