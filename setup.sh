cb26e71f72b7aad7ff20a968254309c14becd0e5
Kinator <behani.julien@gmx.fr>
2026-03-02 11:12:09 +0100

    chore: updated submodule setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 
+sudo apt install openjdk-25-jdk python3.13 love git pip python3-pygame -y
+
+git submodule init --recursive
+git submodule update --recursive
 sudo apt install openjdk-25-jdk python3.13 python3.13-venv love git pip python3-pygame -y
 
 git submodule update --init --recursive
