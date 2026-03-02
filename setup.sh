6c7650926fa8c2a8bea166f38aa8bd8d24609f43
Kinator <behani.julien@gmx.fr>
2026-03-02 13:50:07 +0100

    init

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-#!bin/bash
+#!/bin/bash
 
-sudo apt install openjdk-25-jdk python3.13 love git pip python3-pygame -y
+sudo apt install openjdk-25-jdk python3.13 python3.13-venv love git pip python3-pygame -y
 
-git submodule init --recursive
-git submodule update --recursive
+git submodule update --init --recursive
