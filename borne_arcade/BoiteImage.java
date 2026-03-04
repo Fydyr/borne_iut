d5179786c5a72de0e832e02f7b1a2ced620de5ec
Kinator <48886371+kinator@users.noreply.github.com>
2026-03-04 15:05:33 +0100

    fix: image null

--- borne_arcade/BoiteImage.java
+++ borne_arcade/BoiteImage.java
@@ -12,13 +12,12 @@ public class BoiteImage extends Boite{
     BoiteImage(Rectangle rectangle, String image) {
 	super(rectangle);
         Path filePath = Paths.get(image+"/photo_small.png");
-	if (Files.exists(filePath)) {
-	    this.image = new Texture(image+"/photo_small.png", new Point(760, 648));
-	    //this.image.setTaille(400, 320);
-	} else {
-	    this.image.setImg("img/synave.png");
-	}
-	this.image = new Texture(image+"/photo_small.png", new Point(760, 648));
+		if (Files.exists(filePath)) {
+			this.image = new Texture(image+"/photo_small.png", new Point(760, 648));
+			//this.image.setTaille(400, 320);
+		} else {
+			this.image = new Texture("./img/synave.png", new Point(760, 648));
+		}
     }
 
     public Texture getImage() {
@@ -27,11 +26,11 @@ public class BoiteImage extends Boite{
 
     public void setImage(String chemin) {
         Path filePath = Paths.get(chemin+"/photo_small.png");
-	if (Files.exists(filePath)) {
-	    this.image.setImg(chemin+"/photo_small.png");
-	    //this.image.setTaille(400, 320);
-	} else {
-	    this.image.setImg("./img/synave.png");
-	}
+		if (Files.exists(filePath)) {
+			this.image.setImg(chemin+"/photo_small.png");
+			//this.image.setTaille(400, 320);
+		} else {
+			this.image.setImg("./img/synave.png");
+		}
     }
 }
